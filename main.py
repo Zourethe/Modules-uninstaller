2694313afaa0f0440858738284623ed53816f60b
Zourethe <96656100+Zourethe@users.noreply.github.com>
2023-07-17 09:47:00 -0300

    Updated

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-# Modules.
+'''
+                            Pip Libraries Uninstaller
+    
+    This is the main file of the script, the one that will uninstall the libraries.
+
+    Author: Zourethe
+    Date: July, 17, 2023
+'''
+
+# Libraries imports.
 from os import popen
 
-# Variables.
+# Variables definition.
 modules_list = popen('pip freeze')
 data = modules_list.read().split('\n')
 data.remove('')
 data_len = len(data)
 
-# Loop to uninstall each name.
+# Loop to uninstall each library.
 for x in range(data_len):
     log = popen('pip uninstall {} -y'.format(data[x]))
     output = log.read()
